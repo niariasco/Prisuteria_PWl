4e6ff289c9b0ab7760335c641faf73ffcbcbea75
niariasco <niariasco@est.utn.ac.cr>
2025-07-04 10:26:05 -0600

    FIX : Prisilla me compartio las promociones para poder actuializar esta rama

--- appPrisuteria/src/services/ProductoService.js
+++ appPrisuteria/src/services/ProductoService.js
@@ -16,6 +16,61 @@ class ProductoService {
   getProductoById(ProductoId){
     return axios.get(BASE_URL+'/'+ProductoId);
   }
+ obtenerProductosConPromociones() {
+    return axios
+      .get(`${BASE_URL}/obtenerProductosPromocion`)
+      .then(response => {
+        if (response.data.success) {
+          return response.data.data;
+        } else {
+          throw new Error(response.data.message || 'Error al obtener promociones');
+        }
+      });
+  }
+
+
+
+  //Obtener peliculas por tienda
+  //localhost:81/apiProducto/Producto/ProductosByShopRental/1
+ // getProductoByShopRental(ShopRentalId){
+ //   return axios.get(BASE_URL+'/ProductosByShopRental/'+ShopRentalId);
+ // }
+
+  createProducto(Producto) {
+    return axios.post(BASE_URL, JSON.stringify(Producto));
+  }
+
+  updateProducto(Producto) {
+    return axios({
+      method: 'put',
+      url: BASE_URL,
+      data: JSON.stringify(Producto)
+
+    })
+  }
+  
+}
+
+export default new ProductoService();
+
+/*import axios from 'axios';
+//http://localhost:81/apiProducto/Producto/
+const BASE_URL = import.meta.env.VITE_BASE_URL + 'producto';
+class ProductoService {
+  //Definición para Llamar al API y obtener el listado de películas
+
+  //Listas peliculas
+  //localhost:81/apiProducto/Producto
+  
+  getAllProductos() {
+    return axios.get(BASE_URL);
+  }
+  
+  //Obtener pelicula
+  //localhost:81/apiProducto/Producto/1
+  getProductoById(ProductoId){
+    return axios.get(BASE_URL+'/'+ProductoId);
+  }
   //Obtener peliculas por tienda
   //localhost:81/apiProducto/Producto/ProductosByShopRental/1
  // getProductoByShopRental(ShopRentalId){
@@ -37,3 +92,4 @@ class ProductoService {
 }
 
 export default new ProductoService();
+*/
