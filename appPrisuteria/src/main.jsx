60849f956068e60088207123ea662e389253c8ab
priscillaSanchez <priscillasb@gmail.com>
2025-08-17 20:57:03 -0600

    FIX:  Pagina pago, registro y main

--- appPrisuteria/src/main.jsx
+++ appPrisuteria/src/main.jsx
@@ -37,12 +37,7 @@ import { CreatePromocion } from "./components/Promociones/CreatePromocion";
 import { UpdatePromocion } from "./components/Promociones/UpdatePromocion";
 import { ListaCarrito } from "./components/Carrito/ListaCarrito";
 import { RegistrarPedido } from "./components/Orders/RegistrarPedido";
-
-
-
-
-
-
+import PagoPedido from "./components/Orders/PagoPedido";
 
 const rutas = createBrowserRouter([
   {
@@ -92,14 +87,13 @@ const rutas = createBrowserRouter([
       { path: '/orden', element: <ListOrders /> },
       { path: '/orden/:id', element: <DetalleOrder /> },
       { path: '/cart', element: <ListaCarrito /> },
-     { path: '/registrar-pedido', element: <RegistrarPedido /> }
+     { path: '/registrar-pedido', element: <RegistrarPedido /> },
+      { path: '/pago-pedido', element: <PagoPedido/> }
 
       
     ]
   }
 ]);
-
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
